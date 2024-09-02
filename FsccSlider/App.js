8eebbfac16b92614b2395b9bbdb48147b88f1f43
MazvydasV <mvaicekauskas@inbox.lt>
2024-09-02 20:52:51 +0300

    feat/2/install-slider-package

--- FsccSlider/App.js
+++ FsccSlider/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import SliderFscc from './src/components/SliderFscc'
+
 
 export default function App() {
+  const handleSlider = (newValue) => {
+    console.log('Changing slider', newValue)
+  }
+
   return (
     <View style={styles.container}>
       <Text>FSCC SLIDER!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      <SliderFscc initialValue={0.5} min={0} max={100} step={1} onChange={handleSlider} />
     </View>
   );
 }
