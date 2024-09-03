5a39ec9af4e8cff3c86b118c745a03227fc26132
MazvydasVaicekauskas <117346024+MazvydasVaicekauskas@users.noreply.github.com>
2024-09-03 08:22:03 +0300

    feat/4/add-types

--- FsccSlider/App.tsx
+++ FsccSlider/App.tsx
@@ -1,19 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react'
-import SliderFscc from './src/components/SliderFscc'
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import SliderFscc from "./src/components/SliderFscc";
 
 export default function App() {
-  const handleSlider = (newValue) => {
-    console.log('Changing slider', newValue)
-  }
+  const handleSlider = (newValue: number[]) => {
+    console.log("Changing slider", newValue);
+  };
 
   return (
     <View style={styles.container}>
       <Text>FSCC SLIDER!</Text>
       {/* <StatusBar style="auto" /> */}
-      <SliderFscc initialValue={0.5} min={0} max={100} step={1} onChange={handleSlider} />
+      <SliderFscc
+        initialValue={0.5}
+        min={0}
+        max={100}
+        step={1}
+        onChange={handleSlider}
+      />
     </View>
   );
 }
@@ -21,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- FsccSlider/src/components/SliderFscc.tsx
+++ FsccSlider/src/components/SliderFscc.tsx
@@ -1,42 +1,56 @@
-import React, { useState } from 'react';
-import { Slider } from '@miblanchard/react-native-slider';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from "react";
+import { Slider } from "@miblanchard/react-native-slider";
+import { StyleSheet, View, Text } from "react-native";
+import { SliderOnChangeCallback } from "@miblanchard/react-native-slider/lib/types";
 
-const SliderFscc = ({ initialValue = 0.2, min = 0, max = 1, step = 0.1, onChange }) => {
-    const [value, setValue] = useState(initialValue);
+const SliderFscc = ({
+  initialValue = 0.2,
+  min = 0,
+  max = 1,
+  step = 0.1,
+  onChange,
+}: {
+  initialValue?: number;
+  min?: number;
+  max?: number;
+  step?: number;
+  onChange?: SliderOnChangeCallback;
+}) => {
+  const [value, setValue] = useState(initialValue);
 
-    const handleValueChange = (newValue) => {
-        setValue(newValue);
-        if (onChange) {
-            onChange(newValue);
-        }
-    };
+  const handleValueChange = (newValueArray: number[]) => {
+    const newValue = newValueArray[0];
+    setValue(newValue);
+    if (onChange) {
+      onChange(newValueArray, 0);
+    }
+  };
 
-    return (
-        <View style={styles.container}>
-            <Slider
-                value={value}
-                onValueChange={handleValueChange}
-                minimumValue={min}
-                maximumValue={max}
-                step={step}
-                thumbTintColor="#f50"
-                minimumTrackTintColor="#f50"
-                maximumTrackTintColor="#d3d3d3"
-            />
-            <Text>Value: {value}</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Slider
+        value={value}
+        onValueChange={handleValueChange}
+        minimumValue={min}
+        maximumValue={max}
+        step={step}
+        thumbTintColor="#f50"
+        minimumTrackTintColor="#f50"
+        maximumTrackTintColor="#d3d3d3"
+      />
+      <Text>Value: {value}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginLeft: 10,
-        marginRight: 10,
-        alignItems: 'stretch',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
 });
 
 export default SliderFscc;
